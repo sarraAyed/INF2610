c9d4c374c476b977b88d87a3a7411837d79eeecf
Sarra Ayed <sarrah.ayed@gmail.com>
2019-11-28 20:56:13 -0500

    tp4

--- tp3/commlab-handout/puzzle3.c
+++ tp3/commlab-handout/puzzle3.c
@@ -24,42 +24,49 @@ void puzzle3() {
     if (fork()==0)
     {
         int file = open("./puzzle3/tmpfile", O_WRONLY);
+        //dup2(file, STDERR_FILENO);
+        //dup2( STDERR_FILENO,STDOUT_FILENO);
         dup2(file, STDERR_FILENO);
-        dup2( STDERR_FILENO, STDOUT_FILENO);
-
+        dup2(file, STDOUT_FILENO);
         execl("./puzzle3/exc1", "exc1", NULL);
 
     }
     wait(NULL);
 
-    int fd[2];
-    pipe(fd);
+    
     if(fork() ==0)
     {
-        close(fd[0]);
-    //     dup2(fd[1], STDERR_FILENO);
-    //     close(fd[1]);
-    //     dup2(STDERR_FILENO, STDOUT_FILENO);
-    //     execl("./puzzle3/exc2", "exc2", NULL);
-    }
-    // else if(fork()==0)
-    // {
-    //     close(fd[1]);
-    //     dup2(fd[0], STDERR_FILENO);
-    //     close(fd[0]);
-    //     dup2(STDERR_FILENO, STDOUT_FILENO);
-        
-    //     execl("./puzzle3/exc3", "exc3", NULL);
-    // }
-    while(wait(NULL));
-    close(fd[0]);
-    close(fd[1]);
+        int fd[2];
+        pipe(fd);
+        if (fork() == 0)
+        {
+            close(fd[0]);
+            //dup2( STDERR_FILENO, STDOUT_FILENO);
+            dup2(fd[1], STDERR_FILENO);
+            dup2(fd[1], STDOUT_FILENO);
+            close(fd[1]);
+            execl("./puzzle3/exc2", "exc2", NULL);
+        }
+        else 
+        {
+            close(fd[1]);
 
+            dup2(fd[0], STDIN_FILENO);
+            dup2(STDOUT_FILENO, STDERR_FILENO);
+            close(fd[0]);
+            execl("./puzzle3/exc3", "exc3", NULL);
+        }
+        wait(NULL);
+            
+    }
+  
+    wait(NULL);
     if(fork()==0)
     {
-        int file = open("./puzzle3/tmpfile", O_WRONLY);
+        int file = open("./puzzle3/tmpfile", O_RDONLY);
+        //if(file < 0) printf("YP, WE GOT A PROBLEM HERE");
         dup2(file, STDIN_FILENO);
-        // execl("./puzzle3/exc4", "exc4", NULL);
+        execl("./puzzle3/exc4", "exc4", NULL);
     }
     wait(NULL);
   
--- tp4/synclab-handout/installer.c
+++ tp4/synclab-handout/installer.c
@@ -17,12 +17,86 @@
 // Si besoin, définissez ici des types de structures et/ou
 // des variables
 // -------------------------------------------------------
+struct installAttri 
+{
+    int packageNum;
+    struct management_data* md;
+};
+
+
+char* namePackage [10] = {
+    "good-1.17", "swacheb-8.16", "libslenkopt-3.8", "rub-2.3","libkut-5.9",
+    "bulik-2.19", "libvank-7.2",
+    "liblog-7.14","libgleg-2.5",
+    "flissect-0.0"
+};
 
 // -------------------------------------------------------
 
 // TODO
 // Si besoin, définissez ici des fonctions supplémentaires
 // -------------------------------------------------------
+void* pthreadErrorManagement(void* arg)
+{
+    struct installAttri * cleanUpPar = (struct installAttri *) arg; 
+    doPackageCleanup(cleanUpPar->packageNum, cleanUpPar->md);
+    switch(cleanUpPar->packageNum)
+    {
+        case 0 :
+            break;
+        case 1 :
+            pthread_cancel(cleanUpPar->md->tids[0]);
+            break;
+        case 2 :
+            pthread_cancel(cleanUpPar->md->tids[0]);
+            break;
+        case 3 :
+            pthread_cancel(cleanUpPar->md->tids[0]);
+            break;
+        case 4 :
+            pthread_cancel(cleanUpPar->md->tids[1]);
+            pthread_cancel(cleanUpPar->md->tids[2]);
+            pthread_cancel(cleanUpPar->md->tids[3]);
+            break;
+        case 5 :
+            pthread_cancel(cleanUpPar->md->tids[0]);
+            pthread_cancel(cleanUpPar->md->tids[1]);
+            pthread_cancel(cleanUpPar->md->tids[3]);
+            break;
+        case 6 :
+            pthread_cancel(cleanUpPar->md->tids[1]);
+            pthread_cancel(cleanUpPar->md->tids[2]);
+            pthread_cancel(cleanUpPar->md->tids[3]);
+            break;
+        case 7 :
+            pthread_cancel(cleanUpPar->md->tids[0]);
+            pthread_cancel(cleanUpPar->md->tids[5]);
+            break;
+        case 8 :
+            pthread_cancel(cleanUpPar->md->tids[4]);
+            pthread_cancel(cleanUpPar->md->tids[5]);
+            break;
+        case 9 :
+            pthread_cancel(cleanUpPar->md->tids[5]);
+            pthread_cancel(cleanUpPar->md->tids[1]);
+            break;
+        default :
+            break;
+
+    }
+    return NULL;
+}
+
+void* install (void* arg)
+{
+    
+    
+    struct installAttri* att = ( struct installAttri *) arg;
+    threadedPackageInstaller(att->packageNum, att->md);
+   
+    return NULL;
+}
+
 
 // -------------------------------------------------------
 
@@ -32,7 +106,21 @@
  */
 void initializeManagementData(struct management_data *md) {
     // TODO (Q2, Q3)
-    
+    md->downloadSemaphore = malloc(sizeof(sem_t));
+   // md->canInstallPackage = malloc(10*sizeof(sem_t));
+    sem_init(md->downloadSemaphore, 0, 3);
+    for (int i = 0 ; i <10 ; i++)
+    {
+        md->canInstallPackage[i] = malloc(sizeof(sem_t));
+        if((i == 9) || (i == 8) || (i == 7) || ((i == 6)) )
+        {
+            sem_init(md->canInstallPackage[i], 0, 1);
+        }
+        else 
+        {
+            sem_init(md->canInstallPackage[i], 0, 0);
+        }
+    }
 }
 
 /*
@@ -41,7 +129,13 @@ void initializeManagementData(struct management_data *md) {
  */
 void cleanupManagementData(struct management_data *md) {
     // TODO (Q2, Q3)
-    
+    sem_destroy(md->downloadSemaphore);
+    free(md->downloadSemaphore );
+    for (int i = 0 ; i < 10 ; i++)
+    {
+        sem_destroy(md->canInstallPackage[i]);
+        free(md->canInstallPackage[i]);
+    }
 }
 
 /*
@@ -50,7 +144,19 @@ void cleanupManagementData(struct management_data *md) {
  */
 void installPackage(int packageNum, struct management_data *md) {
     // TODO (Q2, Q3)
+    struct installAttri* att = malloc(sizeof(struct installAttri));
+    att->packageNum = packageNum;
+    att->md = md;
+    pthread_cleanup_push( (void*)pthreadErrorManagement, (void*)att);
     
+
+    sem_wait(md->downloadSemaphore);
+    doPackageDownload(namePackage[packageNum], md);
+    sem_post(md->downloadSemaphore);
+
+    sem_wait(md->canInstallPackage[packageNum]);
+    doPackageInstall(namePackage[packageNum], md);
+   pthread_cleanup_pop (0);
 }
 
 /*
@@ -64,7 +170,8 @@ void installPackage(int packageNum, struct management_data *md) {
  */
 void wakePackage(int wokenPackageNum, struct management_data *md) {
     // TODO (Q3)
-    
+
+
 }
 
 /*
@@ -83,6 +190,42 @@ void wakePackage(int wokenPackageNum, struct management_data *md) {
  */
 void postPackageInstall(int packageNum, struct management_data *md) {
     // TODO (Q3)
+    md->isPackageInstalled[ packageNum] = 1;
+     if ((md->isPackageInstalled[9] == 1) && 
+        (md->isPackageInstalled[8] == 1) &&
+        (md->isPackageInstalled[7] == 1) ) 
+        sem_post(md->canInstallPackage[5]);
+
+    if(md->isPackageInstalled[8]== 1)
+        sem_post(md->canInstallPackage[4]);
+
+    if((md->isPackageInstalled[6]==1)  &&  
+       (md->isPackageInstalled[4]) 
+       )
+        sem_post(md->canInstallPackage[2]);
+
+    if((md->isPackageInstalled[6]==1) && 
+        (md->isPackageInstalled[4]==1)  &&  
+        (md->isPackageInstalled[5] == 1) 
+        )
+        sem_post(md->canInstallPackage[3]);
+
+    if((md->isPackageInstalled[9] == 1)  &&  
+       (md->isPackageInstalled[5] == 1)  &&
+        (md->isPackageInstalled[4]==1)  && 
+        (md->isPackageInstalled[6]==1) 
+        )
+        sem_post(md->canInstallPackage[1]); 
+
+    if((md->isPackageInstalled[1] == 1)  &&  
+        (md->isPackageInstalled[3] == 1)  &&
+        (md->isPackageInstalled[2] == 1) && 
+        (md->isPackageInstalled[5] == 1) && 
+        (md->isPackageInstalled[7] == 1)
+       )
+        sem_post(md->canInstallPackage[0]); 
+
+    
     
 }
 
@@ -96,22 +239,6 @@ void postPackageInstall(int packageNum, struct management_data *md) {
  *   installer
  */
 
-struct installAttri 
-{
-    int packageNum;
-    struct management_data *md;
-};
-
-void* install (void* arg)
-{
-    struct installAttri* att ;
-    att = calloc(1, sizeof(struct installAttri )); 
-    att = ( struct installAttri *) arg;
-    threadedPackageInstaller(att->packageNum, att->md);
-    pthread_exit(NULL);
-}
-
-
 void installer(struct management_data *md) {
     // NE PAS MODIFIER
     // --------------------------
@@ -122,18 +249,22 @@ void installer(struct management_data *md) {
     // TODO (Q1)
     // Lancer les fils d'exécution
     // ---------------------------
-    for(int i=0; i<10;i++)
+    struct installAttri * att[10] ;
+    for (int i= 0 ; i < 10; i++)
     {
-        struct installAttri* att ;
-        att = calloc(1, sizeof(struct installAttri )); 
-        att->packageNum =i;
-        att->md = calloc(1, sizeof(struct management_data )); 
-        att->md = md;
-        pthread_create(&md->tids[i], NULL, install,(void*) att);
+        att[i] = calloc(1,  sizeof(struct installAttri ));
+        att[i]->packageNum = i;
+        att[i]->md = md;
+        pthread_create(&md->tids[i], NULL, (void*)install , (void*) (att[i]));
+    }
+    for (int i = 0 ; i <10 ; i++)
+    {
         pthread_join(md->tids[i], NULL);
     }
-
-
+    for (int i = 0 ; i<10 ; i++)
+    {
+        free(att[i]);
+    }
     // ---------------------------
 
     // NE PAS MODIFIER
