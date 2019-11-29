d7978f66752edc67c3dd30ec5b20a62e4a9f8b45
Sarra Ayed <sarrah.ayed@gmail.com>
2019-11-29 17:10:10 -0500

    13.0 dans le TP5

--- tp5/memlab-handout/memsim.c
+++ tp5/memlab-handout/memsim.c
@@ -7,11 +7,11 @@
 #include "memsim.h"
 #include "memsim/libmemsim.h"
 #include "libmemlab.h"
-
+#include <stdbool.h>
 // TODO
 // Si besoin, ajoutez ici les directives d'inclusion
 // -------------------------------------------------
-
+#include <stdlib.h>
 // -------------------------------------------------
 
 // TODO
@@ -26,7 +26,7 @@
  */
 unsigned long getPageOffset(unsigned long addr) {
     // TODO
-    return 0;
+    return addr%2048;
 }
 
 /*
@@ -35,7 +35,7 @@ unsigned long getPageOffset(unsigned long addr) {
  */
 unsigned long getPageNumber(unsigned long addr) {
     // TODO
-    return 0;
+    return addr>>11;
 }
 
 /*
@@ -44,7 +44,7 @@ unsigned long getPageNumber(unsigned long addr) {
  */
 unsigned long getStartPageAddress(unsigned long pageNumber) {
     // TODO
-    return 0;
+    return pageNumber<<11;
 }
 
 /*
@@ -55,7 +55,41 @@ unsigned long getStartPageAddress(unsigned long pageNumber) {
  */
 struct paging_system_state *initMemoryState() {
     // TODO
-    return (struct paging_system_state *)0;
+     struct paging_system_state* pss = malloc (sizeof(struct paging_system_state));
+
+    struct tlb* TLB = malloc (sizeof(struct tlb));
+    TLB->pageNumbers = calloc(16, sizeof(unsigned long));
+    TLB->frameNumbers =  calloc (16,sizeof(unsigned long));
+    TLB->lastAccessTimestamps = calloc (16,sizeof(int));
+    TLB->entryCreationTimestamps = calloc (16,sizeof(unsigned int));
+    TLB->isUsed = calloc(16,sizeof(char));
+
+    for (int i = 0 ; i< 16 ; i++){
+       TLB->isUsed[i] =0;
+    }
+
+    struct pt* PT = malloc(sizeof(struct pt));
+    PT->frameNumbers = calloc(512,sizeof(unsigned long));
+    PT->isValid = calloc(512,sizeof(char));
+
+    for (int i = 0 ; i< 512 ; i++){
+       PT->isValid[i] =0;
+    }
+
+    struct memory* memoire = malloc (sizeof(struct memory));
+    memoire->pageNumbers=calloc(207,sizeof(unsigned long));
+    memoire->lastAccessTimestamps= calloc(207,sizeof(unsigned int));
+    memoire->entryCreationTimestamps = calloc (207,sizeof(unsigned int));
+    memoire->isUsed = calloc(207,sizeof(char));
+    for (int i = 0 ; i< 207 ; i++){
+        memoire->isUsed[i] =0;
+    }
+
+    pss->tlb = TLB;
+    pss->pt = PT;
+    pss->mem = memoire;
+    return (pss);
+    
 }
 
 /*
@@ -71,7 +105,18 @@ struct paging_system_state *initMemoryState() {
 void lookupInTLB(struct paging_system_state *ms,
                  struct memory_request *mr) {
     // TODO
-
+    unsigned long pageNumber = getPageNumber(mr->virtualAddr);
+    for (int i = 0 ; i < 16 ; i++)
+    {
+        if (ms->tlb->pageNumbers[i] == pageNumber && ms->tlb->isUsed[i] == 1) 
+        {
+            mr->wasFoundInTLB = 1;
+            mr->physicalAddr =  getStartPageAddress(ms->tlb->frameNumbers[i]) + getPageOffset(mr->virtualAddr);
+            ms->tlb->lastAccessTimestamps[i] = mr->timestamp;
+            ms->mem->lastAccessTimestamps[ms->tlb->frameNumbers[i]-455] = mr->timestamp;
+            break;
+        }
+    }
 }
 
 /*
@@ -88,7 +133,14 @@ void lookupInTLB(struct paging_system_state *ms,
 void lookupInPT(struct paging_system_state *ms,
                 struct memory_request *mr) {
     // TODO
-
+    unsigned long pageNumber = getPageNumber(mr->virtualAddr);
+    if(ms->pt->isValid[pageNumber] == 1)
+    {
+        mr->physicalAddr =  getStartPageAddress(ms->pt->frameNumbers[pageNumber])+getPageOffset(mr->virtualAddr);
+        ms->mem->lastAccessTimestamps[ms->pt->frameNumbers[pageNumber]-455] = mr->timestamp;
+    }
+    else 
+        mr->wasPageFault = -1 ;
 }
 
 /*
@@ -108,7 +160,51 @@ void lookupInPT(struct paging_system_state *ms,
 void addToTLB(struct paging_system_state *ms,
               struct memory_request *mr) {
     // TODO
+    unsigned int frame = -1; 
+    for (int i = 0 ; i < 16 ; i++)
+    {
+        if(ms->tlb->isUsed[i] == 0)
+        {
+            frame =  i+455;
+            ms->pt->frameNumbers[getPageNumber(mr->virtualAddr)]=frame;
+            
+            ms->tlb->pageNumbers[i] = getPageNumber(mr->virtualAddr);
+            ms->tlb->frameNumbers[i] = frame;
+            ms->tlb->lastAccessTimestamps[i] = mr->timestamp;
+            ms->tlb->entryCreationTimestamps[i] = mr->timestamp;
+            ms->tlb->isUsed[i] = 1;
+            ms->mem->lastAccessTimestamps[frame] = mr->timestamp;
+            break;
+        }
+    }
+    if(frame == -1)
+    {
+        unsigned int leastRecentlyUsed = ms->tlb->lastAccessTimestamps[0]; 
+        int indexLRU = 0 ;
+        for (int i = 1 ; i < 16 ; i++)
+        {
+            if (ms->tlb->lastAccessTimestamps[i] < leastRecentlyUsed )
+            {
+                leastRecentlyUsed = ms->tlb->lastAccessTimestamps[i];
+                indexLRU = i;
+            }
+                
+        }
 
+        frame =  indexLRU+455;
+        ms->pt->frameNumbers[getPageNumber(mr->virtualAddr)]=frame;
+        ms->pt->isValid[getPageNumber(mr->virtualAddr)]=1;
+        
+        ms->tlb->pageNumbers[indexLRU] = getPageNumber(mr->virtualAddr);
+        ms->tlb->frameNumbers[indexLRU] = frame;
+        ms->tlb->lastAccessTimestamps[indexLRU] = mr->timestamp;
+        ms->tlb->entryCreationTimestamps[indexLRU] = mr->timestamp;
+        ms->tlb->isUsed[indexLRU] = 1;
+        ms->mem->lastAccessTimestamps[frame] = mr->timestamp;
+
+    }
+    
+    
 }
 
 /*
@@ -127,6 +223,104 @@ void addToTLB(struct paging_system_state *ms,
 void getPageIntoMemory(struct paging_system_state *ms,
                        struct memory_request *mr) {
     // TODO
+    // bool fullMem = true;
+    // int indexEmpty = 0 ; 
+    // int indexReplace = -1;
+    
+    // unsigned long oldPage;
+    // unsigned long newPage;
+
+    // unsigned int leastRecentlyUsed = ms->mem->lastAccessTimestamps[0];
+    // int indexLRU = 0 ; 
+    // for (int i = 0 ; i < 207 ; i ++)
+    // {
+    //     if (ms->mem->isUsed[i] == '0')
+    //     {
+    //         fullMem = false;
+    //         indexEmpty = i;
+    //         indexReplace = indexEmpty
+    //         break;
+    //     }
+    //     if (leastRecentlyUsed > ms->mem->lastAccessTimestamps[i])
+    //     {
+    //         leastRecentlyUsed = ms->mem->lastAccessTimestamps[i];
+    //         indexLRU = -1;
+    //     }
+    // }
+
+    // if(fullMem)
+    // {
+    //     indexReplace  = indexLRU;
+    //     oldPage = ms->mem->pageNumbers[indexLRU]; 
+    //     ms->pt->isValid[oldPage] ='0';
+    //     mr->wasEvictionInMemory = 1;
+    //     mr->virtualAddrEvictedFromMemory = getStartPageAddress(oldPage)+getPageOffset(mr->virtualAddr);
+    //     ms->mem->entryCreationTimestamps[oldPage] = mr->timestamp;
+    //     //ajout dans TP
+    //     //Remplir les attrib de mr
+
+    // }
+        
+    //     ms->mem->pageNumbers[indexReplace] = getPageNumber(mr->virtualAddr);
+    //     ms->mem->lastAccessTimestamps[indexReplace] = mr->timestamp;
+    //     ms->mem->entryCreationTimestamps[indexReplace] = mr->timestamp;
+    //     ms->mem->isUsed[indexReplace] = '1';
+    //     newPage = ms->mem->pageNumbers[indexReplace];
+
+    //     ms->pt->frameNumbers[newPage] = indexReplace; //if it doesn't work try to remove the 455
+    //     ms->pt->isValid[newPage] = '1' ; 
+        
+    //     unsigned long adressePhysique = getStartPageAddress(0xe3800+newPage)|getPageOffset(mr->virtualAddr);
+    //     mr->physicalAddr = adressePhysique;
+    bool isfull = true;
+    int i;
+    for ( i = 0 ; i< 207 ; i++) {
+        if (ms->mem->isUsed[i] != 1){
+            isfull = false;
+            break ; 
+        } 
+    }
+
+    if(!isfull){
+        mr->physicalAddr = (getStartPageAddress(i+455)) | getPageOffset(mr->virtualAddr);
+        ms-> mem->entryCreationTimestamps[i] = mr->timestamp;
+        ms-> mem->lastAccessTimestamps[i] = mr->timestamp;
+        ms->mem->pageNumbers[i] = getPageNumber(mr->virtualAddr);
+        ms->pt->frameNumbers[getPageNumber(mr->virtualAddr)] = i+455;
+        ms->pt->isValid[getPageNumber(mr->virtualAddr)]=1;
+        ms->mem->isUsed[i] = 1;
+    }else{
+        
+       
+        int size = 207;
+        int lastAccessElem = 0 ;
+        int valueAcess = ms->mem->lastAccessTimestamps[0];
+        for (int j = 0 ; j < size ; j++){
+            if (ms->mem->lastAccessTimestamps[j]<valueAcess){
+                lastAccessElem = j ;
+                valueAcess = ms->mem->lastAccessTimestamps[j];
+            }
+        }
+
+        mr->wasEvictionInMemory = 1;
+        ms->pt->isValid[ms->mem->pageNumbers[lastAccessElem]]=0;
+
+        mr->virtualAddrEvictedFromMemory = (getStartPageAddress(ms->mem->pageNumbers[lastAccessElem])|getPageOffset(mr->virtualAddr));
+
+
+        ms->mem->pageNumbers[lastAccessElem] = getPageNumber(mr->virtualAddr);
+
+        ms->mem->lastAccessTimestamps[lastAccessElem] = mr->timestamp;
+        ms->mem->entryCreationTimestamps[lastAccessElem] =  mr->timestamp;
+
+        unsigned long elementToReplace = ms->mem->pageNumbers[lastAccessElem];
+
+        mr->physicalAddr = getStartPageAddress(455+lastAccessElem) | getPageOffset(mr->virtualAddr);
+        ms->pt->frameNumbers[getPageNumber(mr->virtualAddr)] = lastAccessElem;
+        ms->pt->isValid[getPageNumber(mr->virtualAddr)]=1;
+        // ms-> mem->entryCreationTimestamps[i] = mr->timestamp;
+        // ms-> mem->lastAccessTimestamps[i] = mr->timestamp;
+   }
 
 }
 
@@ -169,5 +363,21 @@ void processMemoryRequest(struct paging_system_state *ms,
  */
 void cleanMemoryState(struct paging_system_state *ms) {
     // TODO
+    free(ms->tlb->entryCreationTimestamps);
+    free(ms->tlb->isUsed);
+    free(ms->tlb->frameNumbers);
+    free(ms->tlb->lastAccessTimestamps);
+    free(ms->tlb);
+    
+    free(ms->pt->frameNumbers);
+    free(ms->pt->isValid);
+    free(ms->pt);
 
+    free(ms->mem->pageNumbers);
+    free(ms->mem->lastAccessTimestamps);
+    free(ms->mem->entryCreationTimestamps);
+    free(ms->mem->isUsed);
+    free(ms->mem);
+
+    free(ms);
 }
